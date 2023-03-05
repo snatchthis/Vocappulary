e6c73d55b41c1b44a3f6fc0b58127bf7fcf44c12
Piotr Boracki <piotr.boracki@gmail.com>
2023-03-05 18:23:03 +0100

    Initial commit

---
+++ Vocappulary/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Vocappulary;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
